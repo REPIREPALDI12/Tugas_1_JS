a074882e9a4fe22adb78cf7bcc1e007f08978481
REPI REPALDI <repirepaldi12011999@gmail.com>
2023-02-02 10:47:53 +0700

    Tugas  1 JS
    Tugas  1 JS Untuk Niomic

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Tugas_1_JS
+
+# Studi Kasus
+
+- gambar
+  !{img 1}(StudiKasus.png)
---
+++ Tugas1.js
@@ -0,0 +1,7 @@
+var msg1 ="REPI";
+let msg2 ="REPALDI";
+const msg3 ="24";
+
+console.log(msg1);
+console.log(msg2);
+console.log(msg3);
